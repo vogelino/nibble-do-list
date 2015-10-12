1467d5b6855ffc77f297a3e323e6f16b7779d603
Lucas Vogel <contact@vogelino.ch>
2015-10-12 10:55:14 +0200

    Added the ground structure for a simple todo app

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,107 @@
-$(document).ready(function () {
-	var socket = new io();
-	socket.connect('http://localhost:3000', {
-		autoConnect: true
-	});
+var socket = new io();
+socket.connect('http://localhost:3000', {
+	autoConnect: true
+});
 
-	$('#test').click(function(e) {
-		e.preventDefault();
-		socket.send({
-			type: 'TASK_COMPLETED'
+var TodoItem = React.createClass({
+	propTypes: {
+		title: React.PropTypes.string.isRequired,
+		id: React.PropTypes.number.isRequired,
+		done: React.PropTypes.bool.isRequired,
+		onTodoChange: React.PropTypes.func.isRequired
+	},
+	onChange: function (e) {
+		e.stopPropagation();
+		this.props.onTodoChange(Object.assign({}, {
+			title: this.refs.title.value,
+			id: this.props.id,
+			done: !this.refs.done.value
+		}));
+	},
+	render: function() {
+		return (
+			React.createElement('li', {
+				className: 'todo-item ' +
+					(this.props.done ? 'todo-item-done' : '')
+			},
+				React.createElement('input', {
+					type: 'checkbox',
+					value: this.props.id,
+					id: this.props.id,
+					defaultChecked: this.props.done,
+					ref: 'done',
+					onChange: this.onChange
+				}),
+				React.createElement('input', {
+					type: 'text',
+					value: this.props.title,
+					ref: 'title',
+					onChange: this.onChange
+				})
+			)
+		);
+	}
+});
+
+var TodoList = React.createClass({
+	propTypes: {
+		todos: React.PropTypes.array.isRequired,
+		onTodoChange: React.PropTypes.func.isRequired
+	},
+	render: function() {
+		var onTodoChange = this.props.onTodoChange;
+		var listOfTodos = this.props.todos.map(function (todo) {
+			return (
+				React.createElement(TodoItem, {
+					title: todo.title,
+					id: todo.id,
+					key: todo.id,
+					done: todo.done,
+					onTodoChange: onTodoChange
+				})
+			);
 		});
+		return (
+			React.createElement('ul', { className: 'todo-list' }, listOfTodos)
+		);
+	}
+});
+
+// Templates
+var TODO_TEMPLATE = { title: '', id: '', done: '', errors: {}}
+
+// Actions
+function markTaskComplete() {
+	socket.send({
+		type: 'TASK_COMPLETED'
 	});
+};
+
+function onTodoChange(changes) {
+	console.log('Todo state updated', changes);
+	var newTodos = [];
+	state.todos.forEach(function (todo) {
+		newTodos.push(todo.id === changes.id ? changes : todo);
+	});
+	setState({ todos: newTodos });
+}
+
+// Model
+var state = {};
+
+function setState(changes) {
+    Object.assign(state, changes);
+    var Component = React.createElement(TodoList, Object.assign({}, state, {
+		onTodoChange: onTodoChange
+	}));
+
+    ReactDOM.render(Component, document.getElementById('app'));
+}
+
+setState({
+	todos: [{
+		title: 'Test todo',
+		id: 1,
+		done: false
+	}]
 });
